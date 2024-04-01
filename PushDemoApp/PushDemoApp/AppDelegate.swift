21de81cde406b42198266547cf1cc567a606247b
haewonios <haewon.dev@gmail.com>
2024-04-02 00:18:12 +0900

    Firebase 관련 설정 추가

--- PushDemoApp/PushDemoApp/AppDelegate.swift
+++ PushDemoApp/PushDemoApp/AppDelegate.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var application: UIApplication?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.application = application
+        
+        // 사용자 알림 권한 요청
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { didAllow, Error in
+            print("didAllow: \(didAllow)")
+        }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        application.registerForRemoteNotifications()
+        
+        FirebaseApp.configure() // 초기화
+        Messaging.messaging().delegate = self
+        
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+                
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
+        }
         
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
@@ -23,3 +47,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.list, .banner, .sound])
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
